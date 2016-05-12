57a172f6b81afc76381b42ae7d1b4141093b27f8
zer0-byte <a_l189906@naver.com>
2016-05-12 21:56:06 +0900

    Add files via upload

---
+++ main/main/java/com/example/april/search/Plus.java
@@ -0,0 +1,13 @@
+package com.example.april.search;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Plus extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plus);
+    }
+}
